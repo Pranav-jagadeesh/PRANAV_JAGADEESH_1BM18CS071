44cacddbe6ee24ce35957d1e308cfd0cac190a4e
Pranav-jagadeesh <53830306+Pranav-jagadeesh@users.noreply.github.com>
2019-08-15 11:02:21 +0530

    PRANAV_JAGADEESH_1BM18CS071
    IMPLEMENTATION OF STACK PROGRAM

--- stack.c
+++ stack.c
@@ -1,58 +1,56 @@
 #include<stdio.h>
-#include<stdbool.h>
-
+#include<process.h>
 #define MAX_SIZE 3
 int item[MAX_SIZE];
 int top=-1;
+// Function to add elements into stack
  void push(int x)
  {
-  if(isfull()==1)
-  printf("error:stack is full");
+  if(top==MAX_SIZE-1)
+  printf("Error:%d cant be inserted into stack as stack is full\n",x);
   else
   {
-   printf("enter number");
-   scanf("%d",&x);
    top=top+1;
    item[top]=x;
   }
  }
+// Function to remove elements into stack with LIFO Technique
  void pop()
  {
   int x;
-  if(isempty()==1)
-  printf("error:stack is empty");
-  else
-  { 
-   x=item[top];
-   top=top-1;
-   printf("%d is removed",x);
-  }
- }
-int isfull()
- {
-  if(top==MAX_SIZE)
-  return 1;
-  else return 0;
- }
- int isempty()
- {
   if(top==-1)
-  return 1;
-  else return 0;
+  printf("error:stack is empty\n");
+  else
+   { 
+    x=item[top];
+    top=top-1;
+    printf("%d is removed\n",x);
+   }
  }
+// Function to print all the elements in stack
  void print()
  {
   int i;
+  if(top==-1)
+  {
+  	printf("Stack is empty-nothing to print\n");
+  }
+  else
+  {
   printf("stack:");
   for(i=0;i<=top;i++) 
   printf("%d ",item[i]);
   printf("\n");
+  }
  }
- void main()
- {
+ int main()
+ { 
   int choice,item;
-  printf("enter choice 1.push 2.pop 3.print stack 4.exit");
-scanf("%d",&choice);
+  // Infinite loop to take multiple choices from user, untill he gives choice as 4
+  while(1)
+  {  
+   printf("enter choice 1.push 2.pop 3.print stack 4.exit\n");
+   scanf("%d",&choice);
 switch(choice)
 {
 case 1:
@@ -60,26 +58,18 @@ printf("Enter the item to be pushed\n");
 scanf("%d",&item);
 push(item);
 break;
-
+  
 case 2: pop();
 break;
 
 case 3: print();
 break;
 
+case 4: exit(0);
+
 default:
-break;
-  //push(10);
- // print();
-  //push(5);
-  //print();
-  //push(2);
- // print();
- // pop();
- // print();
-  //pop();
- // print();
-  //pop();
-  //print();
+printf("Invalid choice - Enter correct choice\n");
  }
+}
+ return 0;
 }
